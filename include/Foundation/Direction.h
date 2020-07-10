10be054a01cac2b26a25cc0a47081ce442b4268c
Jordan Tymburski <hello@jordantymburski.com>
2020-07-09 23:26:14 -0700

    Move Direction enumerator under core namespace

--- include/Foundation/Direction.h
+++ include/Foundation/Direction.h
@@ -4,35 +4,38 @@
  * Enumerator defining a direction on a standard compass axis: North, East, South, West.
  * It is designed to be bitwise combined under one reference, if necessary.
  */
-#ifndef DIRECTION_H
-#define DIRECTION_H
+#ifndef CORE_DIRECTION_H
+#define CORE_DIRECTION_H
 
 #include <cstdint>
 
-enum class Direction : std::uint8_t
+namespace core
 {
-  NONE  = 0,
-  NORTH = 1 << 0,
-  EAST  = 1 << 1,
-  SOUTH = 1 << 2,
-  WEST  = 1 << 3
+  enum class Direction : std::uint8_t
+  {
+    NONE  = 0,
+    NORTH = 1 << 0,
+    EAST  = 1 << 1,
+    SOUTH = 1 << 2,
+    WEST  = 1 << 3
+  };
+
+  constexpr enum Direction operator& (const enum Direction firstValue,
+                                      const enum Direction secondValue)
+  {
+    return static_cast<Direction>(uint8_t(firstValue) & uint8_t(secondValue));
+  }
+
+  constexpr enum Direction operator| (const enum Direction firstValue,
+                                      const enum Direction secondValue)
+  {
+    return static_cast<Direction>(uint8_t(firstValue) | uint8_t(secondValue));
+  }
+
+  constexpr enum Direction operator~ (const enum Direction value)
+  {
+    return static_cast<Direction>(~uint8_t(value));
+  }
 };
 
-constexpr enum Direction operator& (const enum Direction firstValue,
-                                    const enum Direction secondValue)
-{
-  return static_cast<Direction>(uint8_t(firstValue) & uint8_t(secondValue));
-}
-
-constexpr enum Direction operator| (const enum Direction firstValue,
-                                    const enum Direction secondValue)
-{
-  return static_cast<Direction>(uint8_t(firstValue) | uint8_t(secondValue));
-}
-
-constexpr enum Direction operator~ (const enum Direction value)
-{
-  return static_cast<Direction>(~uint8_t(value));
-}
-
-#endif // DIRECTION_H
+#endif // CORE_DIRECTION_H
